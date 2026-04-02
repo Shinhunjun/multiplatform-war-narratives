9e49a7a5a5067528a88064b77d8be067c02e4500
Jun <shj0213@gmail.com>
2026-04-02 10:21:18 -0400

    Remove legacy venezuela-*-discourse/ folders (consolidated into gdelt/, reddit/, tiktok/)

--- venezuela-us-reddit-discourse/preprocessing/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Venezuela-US Reddit Discourse Preprocessing Module.
-"""
-
-from .config import PreprocessConfig
-from .preprocessor import RedditPreprocessor
-
-__all__ = ["PreprocessConfig", "RedditPreprocessor"]
